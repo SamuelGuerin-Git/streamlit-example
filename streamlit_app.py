9bba0a349ffefd666b906d80535e110eb6400f64
JuCoHunon <99874034+JuCoHunon@users.noreply.github.com>
2022-07-02 22:50:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def main():
         st.image(image)
 	st.write("Le projet présenté dans ce streamlit a été développé dans le cadre de la formation Data Scientist de Datascientest.com - Promotion Octobre 2021."
 		 "\n\n"
-		 "L'objectif premier de ce projet est de mettre en application les différents acquis de la formation sur la problématique de prévision météo et plus précisément la question essentielle: va-t-il pleuvoir demain?")
+		 "L'objectif premier de ce projet est de mettre en application les différents acquis de la formation sur la problématique de prévision météo et plus précisément de répondre à une question essentielle: va-t-il pleuvoir demain?")
 	st.image('images/Intro_météo.jpg')
 	st.write("En dehors d'intéresser particulièrement les fabricants de parapluie, on comprend aussi que cette question est essentielle que ce soit dans le domaine du loisirs (gestion des parcs d'attraction), de l'agriculture, le traffic routier, et bien d'autres sujets."
 		 "\n\n"
